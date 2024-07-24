e2ba206b3a92ddc0fd930e99767a903621e3f722
Arjun Praba <Arjun Praba@ArjunPraba>
2024-07-24 09:52:12 +0530

    day second

--- C2TC/src/com/tns/dayone/FirstClass.java
+++ C2TC/src/com/tns/dayone/FirstClass.java
@@ -4,7 +4,19 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int a=1;
+		while(a<5) {
+			System.out.println("hello");
+			a++;
+		}
+		do {
+			System.out.println("hii");
+			a++;		
+			}
+		while(a<10);
+	for(int i=0;i<5;i++) {
+		System.out.println("world");
+	}
 	}
 
 }
